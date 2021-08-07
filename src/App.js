08fad4da3b23569e11e9dd7353a96a005ec73856
abdumutalibov <azimabdumutalibovazim@gmail.com>
2021-08-08 00:06:23 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import './App.css';
+import data from './mock-data.json'
+const App = ()=>{
+  const [contacts,setContacts]= useState(data);
+const [addFormData,setAddFormData] = useState({
+fullName:'',
+address:'',
+phoneNumber:'',
+email:''
+})
+
+
+const handleAddFormChange=(event)=>{
+  event.preventDefault();
+
+  const fieldName = event.target.getAttribute('name')
+  const fieldValue = event.target.value;
+
+  const newFormData={ ...addFormData};
+  
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='app-container'>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Phone Number</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map((contacts)=>(
+        <tr>
+          <td>Jenny Chan</td>  
+          <td>3 waterfoot</td>
+          <td>333-962-7516</td>
+          <td>jenny.chan@email.com</td>
+        </tr>
+         ))}
+      </tbody>
+    </table>
+
+    <h2>Add a Contact</h2>
+    <from>
+    <input
+     type='text' 
+      name='fullName'
+      required='required'
+      placeholder = 'Enter a name...'
+      /> 
+      <input
+      type='text' 
+       name='address'
+       required='required'
+       placeholder = 'Enter a address...'
+       />
+        <input
+     type='text' 
+      name='phoneNumber'
+      required='required'
+      placeholder = 'Enter a phone number...'
+      />
+       <input
+     type='email' 
+      name='email'
+      required='required'
+      placeholder = 'Enter an email...'
+      />
+      <button type='submit'>Add</button>
+    </from>
+  </div>
+  )
 }
 
 export default App;
